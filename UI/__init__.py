874a16187e0660df25407ee911c73c5dc8727acd
Merodac <merodac_github@gelse.net>
2020-08-19 21:36:41 +0200

    asads

--- UI/__init__.py
+++ UI/__init__.py
@@ -4,7 +4,7 @@ from kivy.uix.stacklayout import StackLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.pagelayout import PageLayout
 from kivy.uix.textinput import TextInput
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ObjectProperty
 from kivy.graphics import Color, Rectangle
 from kivy.uix.scatter import Scatter
 from kivy.uix.label import Label
@@ -19,4 +19,14 @@ class MainApp(App):
   def on_pause(self):
     return True
 
+
+class MyAsyncImage(AsyncImage):
+  state = StringProperty("Initialized.")
+
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+    self.state = "Loading."
+
+  def on_load(self, *args):
+    self.state = "Loaded."
 
